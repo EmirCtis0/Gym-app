83571539d57f6856273a786a18f3b6ac2fb9d095
EmirCtis0 <emirinaner@gmail.com>
2024-10-30 16:52:35 +0300

    generator pade revised

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SectionWraper from './SectionWraper'
+import { WORKOUTS } from '../utils/swoldier'
 
 function Header(props){
   const{index,title,description}=props
@@ -15,10 +16,41 @@ function Header(props){
 }
 
 export default function Generator() {
+
+  let showModal = false
+
+
   return (
    <SectionWraper header={"generate your workout"} title=
    {['It\'s','Huge','o\'clock']}>
-    <Header index={'01'} title={'Pick your poison'} description={"Select the workout you wish to endure."}></Header>
+    <Header index={'01'} title={'Pick your poison'} 
+    description={"Select the workout you wish to endure."}></Header>
+
+    <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
+
+    {Object.keys(WORKOUTS).map((type,typeIndex) => {
+      return(
+        <button  className='bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3 rounded-lg'
+        key={typeIndex}>
+          <p className='capitalize'>{type.replaceAll('_',' ')}</p>
+        </button>
+      )
+    })}
+     </div>
+     <Header index={'02'} title={'Lock On Target'} 
+    description={"Select the muscles judged for annihilation"}></Header>
+
+    <div className='bg-slate-950 border border-solid border-blue-400 rounded-lg flex flex-col'>
+      <button className='relative flex items-center py-3 justify-center'>
+        <p>Select muscle groups</p>
+        <i class="fa-solid absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down"></i>
+      </button>
+      {showModal && (
+        <div>
+          modal 
+        </div>
+      )}
+    </div>
    </SectionWraper>
   )
 }
